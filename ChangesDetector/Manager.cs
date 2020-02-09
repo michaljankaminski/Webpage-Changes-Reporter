93c29dc13350546b8f24aeb58b03751f65b6c21a
ponowemu <m.ferfet@ponowemu.pl>
2020-02-09 01:31:56 +0100

    Sitemap comparison

--- ChangesDetector/Manager.cs
+++ ChangesDetector/Manager.cs
@@ -12,9 +12,9 @@ namespace ChangesDetector
             Uri url = new Uri("http://ponowemu.pl");
             IDownloader downloader = new WebpageDownloader();
             var result = downloader.Download(url);
-
+            var result2 = downloader.Download(new Uri("https://sikoraauxilium.com/"));
             IDetector detector = new Detector();
-            detector.Detect(result, result);
+            detector.Detect(result, result2);
 
 
         }
