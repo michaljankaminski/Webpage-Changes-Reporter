23c8eae7c0f549978a41175cfa629cedaa13b897
ponowemu <m.ferfet@ponowemu.pl>
2020-02-09 14:46:09 +0100

    Detecting changes in background service

--- ChangesDetector/Manager.cs
+++ ChangesDetector/Manager.cs
@@ -18,5 +18,12 @@ namespace ChangesDetector
 
 
         }
+        public  void Test2()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.Write(".");
+            }
+        }
     }
 }
