ffcc850ff3cd51e21b2f955c22f2de59ca5d9175
ponowemu <m.ferfet@ponowemu.pl>
2020-02-09 16:15:03 +0100

    Json config file for mail server

--- ChangesDetector/Manager.cs
+++ ChangesDetector/Manager.cs
@@ -2,11 +2,39 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace ChangesDetector
 {
     public class Manager
     {
+        private readonly MailConfiguration _mailConfiguration;
+        public Manager()
+        {
+            MailConfiguration mailConfig;
+            Config(out mailConfig);
+
+            _mailConfiguration = mailConfig;
+        }
+        public void Config(out MailConfiguration mailConfiguration)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var mailConfig = configuration.GetSection("mail");
+
+            mailConfiguration = new MailConfiguration()
+            {
+                HostSmtp = mailConfig.GetSection("hostSmtp").Value,
+                LoginSmtp = mailConfig.GetSection("loginSmtp").Value,
+                MailFromSmtp = mailConfig.GetSection("mailFromSmtp").Value,
+                PasswordSmtp = mailConfig.GetSection("passwordSmtp").Value,
+                PortSmtp = Convert.ToInt32(mailConfig.GetSection("portSmtp").Value),
+                SslSmtp = Convert.ToBoolean(mailConfig.GetSection("sslSmtp").Value)
+            };
+
+        }
         public void Test()
         {
             Uri url = new Uri("http://ponowemu.pl");
@@ -18,7 +46,7 @@ namespace ChangesDetector
 
 
         }
-        public  void Test2()
+        public void Test2()
         {
             for (int i = 0; i <= 10; i++)
             {
